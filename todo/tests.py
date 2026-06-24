a27cf4dba09d4fe6f050b42ab34a436063a85687
1F10250106 <s1f102501068@iniad.org>
2026-06-24 20:51:57 +0900

    Taskモデルにメソッドを再追加

--- todo/tests.py
+++ todo/tests.py
@@ -34,4 +34,19 @@ class TaskModelTestCase(TestCase):
         task = Task(title='task1', due_at=due)
         task.save()
 
+        self.assertFalse(task.is_overdue)
+        
+    def test_is_overdue_past(self):
+        due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
+        current = timezone.make_aware(datetime(2024, 7, 1, 0, 0, 0))
+        task = Task(title='task1', due_at=due)
+        task.save()
+
+        self.assertTrue(task.is_overdue(current))
+
+    def test_is_overdue_none(self):
+        current = timezone.make_aware(datetime(2024, 7, 1, 0, 0, 0))
+        task = Task(title='task2')
+        task.save()
+
         self.assertFalse(task.is_overdue(current))
